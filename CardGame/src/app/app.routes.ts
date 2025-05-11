569d3abe684aad7961ea2787f6bf401fc501874c
Alexis Chaignon <alexis.chaignon@gmail.com>
2025-05-11 20:27:29 +0200

    feat: add card-manager route and fix login router navigate on submit

--- CardGame/src/app/app.routes.ts
+++ CardGame/src/app/app.routes.ts
@@ -2,11 +2,18 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './core/auth/login/login.component';
 import { RegisterComponent } from './core/auth/register/register.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { CardManagerComponent } from './components/card-manager/card-manager.component';
+import { CardListComponent } from './components/card-list/card-list.component';
 
 export const routes: Routes = [
 { path: '', redirectTo: 'login', pathMatch: 'full'},
 { path: 'login', component: LoginComponent },
 { path: 'register', component: RegisterComponent },
-{ path: 'dashboard', component: DashboardComponent },
+{ path: 'dashboard', component: DashboardComponent,
+  children: [
+    { path: '', redirectTo: 'game', pathMatch: 'full' },
+    { path: 'card-manager', component: CardManagerComponent },
+  ],
+  },
 
 ];
