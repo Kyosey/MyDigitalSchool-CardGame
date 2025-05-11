1ca1b41d6c78afd7aa6b6e90fd544982484a5746
Alexis Chaignon <alexis.chaignon@gmail.com>
2025-05-11 21:23:14 +0200

    feat: add game route

--- CardGame/src/app/app.routes.ts
+++ CardGame/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { RegisterComponent } from './core/auth/register/register.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { CardManagerComponent } from './components/card-manager/card-manager.component';
 import { DeckComponent } from './components/deck/deck.component';
+import { GameComponent } from './components/game/game.component';
 
 export const routes: Routes = [
 { path: '', redirectTo: 'login', pathMatch: 'full'},
@@ -13,7 +14,8 @@ export const routes: Routes = [
   children: [
     { path: '', redirectTo: 'game', pathMatch: 'full' },
     { path: 'card-manager', component: CardManagerComponent },
-    { path: 'deck', component: DeckComponent}
+    { path: 'deck', component: DeckComponent},
+    { path: 'game', component: GameComponent},
   ],
   },
 
