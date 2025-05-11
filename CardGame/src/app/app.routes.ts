e7dbe32d1719ed159e25650dfe017b25d7172115
Alexis Chaignon <alexis.chaignon@gmail.com>
2025-05-11 19:15:39 +0200

    feat: create routes to login and register

--- CardGame/src/app/app.routes.ts
+++ CardGame/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './core/auth/login/login.component';
+import { RegisterComponent } from './core/auth/register/register.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+{path: '', redirectTo: 'login', pathMatch: 'full'},
+{ path: 'login', component: LoginComponent },
+{ path: 'register', component: RegisterComponent },
+{ path: 'register', component: RegisterComponent },
+
+];
