dfeb43a67d6aaf9eb44c7b6639811c9e39f61c83
Alexis Chaignon <alexis.chaignon@gmail.com>
2025-05-11 19:15:21 +0200

    feat: create auth service & interceptor

---
+++ CardGame/src/app/core/services/auth.service.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environments';
+import { Observable, of } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+
+interface LoginModel {
+  login: string;
+  password: string;
+}
+
+interface TokenModel {
+  token: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private tokenKey = 'auth_token';
+  private ssoUrl = environment.ssoUrl;
+
+  constructor(private http: HttpClient) {}
+
+  login(loginData: LoginModel): Observable<void> {
+    return this.http.post<TokenModel>(`${environment.ssoUrl}/api/login`, loginData).pipe(
+      tap(response => {
+        this.saveToken(response.token);
+      }),
+      map(() => {})
+    );
+  }
+
+  logout(): void {
+    localStorage.removeItem(this.tokenKey);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem(this.tokenKey);
+  }
+
+  private saveToken(token: string): void {
+    localStorage.setItem(this.tokenKey, token);
+  }
+
+  refreshToken(profileId: string): Observable<void> {
+    return this.http.get<TokenModel>(`${environment.ssoUrl}/token/${profileId}`).pipe(
+      tap(response => {
+        this.saveToken(response.token);
+      }),
+      map(() => {})
+    );
+  }
+
+  isLoggedIn(): boolean {
+    return !!this.getToken();
+  }
+  register(userData: any) {
+    return this.http.put(`${this.ssoUrl}/api/profiles`, userData);
+  }
+
+}
